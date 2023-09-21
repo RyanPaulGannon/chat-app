7dfeab50f6a4ca06b65ac49abc9bd23a94a59448
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-09-21 18:38:02 +0100

    `Style`: Split for mobile view

--- app.vue
+++ app.vue
@@ -1,17 +1,52 @@
 <template>
-  <div style="display: flex; height: 100vh; margin: 0; padding: 0">
-    <div style="flex: 1; height: 100%">
+  <div class="container">
+    <div class="left">
       <Chat />
     </div>
-    <div style="flex: 1; height: 100%">
+    <div class="right">
       <Preview />
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 body {
   margin: 0;
   padding: 0;
+}
+
+.container {
+  display: flex;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+.left {
+  flex: 1;
+  height: 100%;
+}
+
+.right {
+  flex: 1;
+  height: 100%;
+}
+
+@media (max-width: 768px) {
+  .container {
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .left {
+    order: 2;
+  }
+
+  .left,
+  .right {
+    flex: none;
+    width: 100%;
+    height: 50%;
+  }
 }
 </style>
